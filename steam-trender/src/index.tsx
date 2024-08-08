62f07939b221e65f0498feff7affd3caadb16ee6
Lev Kobelev <35657934+evlko@users.noreply.github.com>
2024-08-08 13:08:29 +0300

    [add] sass

--- steam-trender/src/index.tsx
+++ steam-trender/src/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
-import "./styles/main.css";
 import App from "./App";
+import "./styles/custom.css";
+import "./styles/main.css";
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
