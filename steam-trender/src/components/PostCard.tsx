fb52270534cef6a65ff8175448981b04b8ee68be
Lev Kobelev <35657934+evlko@users.noreply.github.com>
2024-08-08 13:09:45 +0300

    [remove] image from card

--- steam-trender/src/components/PostCard.tsx
+++ steam-trender/src/components/PostCard.tsx
@@ -15,15 +15,10 @@ export function PostCard({ post }: PostCardProprs) {
             style={{ textDecoration: "none", color: "inherit" }}
         >
             <div className="card overflow-hidden" style={{ height: "200px" }}>
-                <div className="row g-0">
-                    <div className="col-4 h-100 d-flex align-items-center justify-content-center rounded-start overflow-hidden">
-                        <img loading="lazy" src={post.image} alt={post.title} />
-                    </div>
-                    <div className="col-8">
-                        <div className="card-body">
-                            <h5 className="card-title">{post.title}</h5>
-                            <p className="card-text">{post.description}</p>
-                        </div>
+                <div className="col-12">
+                    <div className="card-body">
+                        <h5 className="card-title">{post.title}</h5>
+                        <p className="card-text">{post.description}</p>
                     </div>
                 </div>
             </div>
