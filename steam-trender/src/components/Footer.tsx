4b88b82076d4c26e6eedebb9a67a9ed259d91901
Lev Kobelev <35657934+evlko@users.noreply.github.com>
2024-08-13 23:38:14 +0300

    [add] status to footer

--- steam-trender/src/components/Footer.tsx
+++ steam-trender/src/components/Footer.tsx
@@ -1,27 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import ApiService from "../api/service";
+import { IStatus } from "../models/status";
 
 const Footer = () => {
+    const [status, setStatus] = useState<IStatus>();
+
+    useEffect(() => {
+        const fetchStatus = async () => {
+            const fetchedStatus = await ApiService.fetchStatus();
+            setStatus(fetchedStatus);
+        };
+
+        fetchStatus();
+    }, []);
+
     return (
-        <footer className="bg-light mt-2">
-            <div className="container text-center pb-3 pt-1">
-                <p className="m-1">Contact us: MAIL</p>
+        <footer className="row pb-3 pt-2 mt-2 mx-0 bg-light text-center">
+            <div className="col-sm-12 col-md-4">
+                <span>
+                    status: <b>{status?.status}</b>
+                </span>
+            </div>
+            <div className="col-sm-12 col-md-4 p-0">
+                <span>
+                    contact us: <b>help [at] steamtrender [dot] com</b>
+                </span>
+            </div>
+            <div className="col-sm-12 col-md-4">
+                <span>
+                    updated: <b>{status?.update.date}</b>
+                </span>
+            </div>
+            <div className="col-sm-12 col-md-4">
                 <a
-                    className="mx-5"
                     href="https://steampowered.com/"
                     target="_blank"
                     rel="noopener noreferrer"
                 >
                     powered by steam
                 </a>
-                |
-                <Link className="mx-5" to="/about">
-                    about
-                </Link>
-                |
-                <Link className="mx-5" to="/privacy">
-                    privacy policy
-                </Link>
+            </div>
+            <div className="col-sm-12 col-md-4">
+                <Link to="/about">about</Link>
+            </div>
+            <div className="col-sm-12 col-md-4">
+                <Link to="/privacy">privacy policy</Link>
             </div>
         </footer>
     );
