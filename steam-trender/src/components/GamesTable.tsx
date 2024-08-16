5539ed9d9acdc9243a4a263128076807f5b1f614
Lev Kobelev <35657934+evlko@users.noreply.github.com>
2024-08-16 21:19:12 +0300

    [fix] tables

--- steam-trender/src/components/GamesTable.tsx
+++ steam-trender/src/components/GamesTable.tsx
@@ -12,58 +12,60 @@ export function GamesTable({ games, limit = 100 }: GamesTableProps) {
     return (
         <small>
             <small>
-                <table className="table table-striped">
-                    <thead>
-                        <tr>
-                            <th scope="col">Title</th>
-                            <th scope="col"></th>
-                            <th scope="col">Reviews Total</th>
-                            <th scope="col">Reviews Score</th>
-                            <th scope="col">Release Date</th>
-                            <th scope="col">Launch Price</th>
-                            <th scope="col">Tags</th>
-                            <th scope="col">Owners Estimated</th>
-                            <th scope="col">Revenue Estimated</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {limitedGames.map((game) => (
-                            <tr key={game.id}>
-                                <td>
-                                    <a
-                                        href={`https://store.steampowered.com/app/${game.appid}`}
-                                    >
-                                        {game.title}
-                                    </a>
-                                </td>
-                                <td>
-                                    <img
-                                        width="150px"
-                                        src={`https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/${game.appid}/header.jpg`}
-                                    ></img>
-                                </td>
-                                <td>
-                                    <NumberFormatter value={game.reviews} />
-                                </td>
-                                <td>{game.reviews_score}%</td>
-                                <td>{game.release_date}</td>
-                                <td>${game.price}</td>
-                                <td>
-                                    {game.tags
-                                        .map((tag) => tag.title)
-                                        .join(", ")}
-                                </td>
-                                <td>
-                                    <NumberFormatter value={game.owners} />
-                                </td>
-                                <td>
-                                    $
-                                    <NumberFormatter value={game.revenue} />
-                                </td>
+                <div className="table-responsive">
+                    <table className="table table-striped">
+                        <thead>
+                            <tr>
+                                <th scope="col">Title</th>
+                                <th scope="col"></th>
+                                <th scope="col">Reviews Total</th>
+                                <th scope="col">Reviews Score</th>
+                                <th scope="col">Release Date</th>
+                                <th scope="col">Launch Price</th>
+                                <th scope="col">Tags</th>
+                                <th scope="col">Owners Estimated</th>
+                                <th scope="col">Revenue Estimated</th>
                             </tr>
-                        ))}
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                            {limitedGames.map((game) => (
+                                <tr key={game.id}>
+                                    <td>
+                                        <a
+                                            href={`https://store.steampowered.com/app/${game.appid}`}
+                                        >
+                                            {game.title}
+                                        </a>
+                                    </td>
+                                    <td>
+                                        <img
+                                            width="150px"
+                                            src={`https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/${game.appid}/header.jpg`}
+                                        ></img>
+                                    </td>
+                                    <td>
+                                        <NumberFormatter value={game.reviews} />
+                                    </td>
+                                    <td>{game.reviews_score}%</td>
+                                    <td>{game.release_date}</td>
+                                    <td>${game.price}</td>
+                                    <td>
+                                        {game.tags
+                                            .map((tag) => tag.title)
+                                            .join(", ")}
+                                    </td>
+                                    <td>
+                                        <NumberFormatter value={game.owners} />
+                                    </td>
+                                    <td>
+                                        $
+                                        <NumberFormatter value={game.revenue} />
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
             </small>
         </small>
     );
--- steam-trender/src/components/OverviewTable.tsx
+++ steam-trender/src/components/OverviewTable.tsx
@@ -10,112 +10,120 @@ interface OverviewTableProps {
 export function OverviewTable({ data }: OverviewTableProps) {
     return (
         <small>
-            <table className="table table-striped">
-                <thead>
-                    <tr>
-                        <th></th>
-                        {data.map((item, index) => (
-                            <th key={index}>{item.title}</th>
-                        ))}
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Games Released</td>
-                        {data.map((item, index) => (
-                            <td key={index}>
-                                <NumberFormatter
-                                    value={item.overview.total_games}
-                                />
-                            </td>
-                        ))}
-                    </tr>
-                    <tr>
-                        <td>Median Reviews</td>
-                        {data.map((item, index) => (
-                            <td key={index}>
-                                <NumberFormatter
-                                    value={item.overview.median_reviews}
-                                />
-                            </td>
-                        ))}
-                    </tr>
-                    <tr>
-                        <td>Median Owners</td>
-                        {data.map((item, index) => (
-                            <td key={index}>
-                                <NumberFormatter
-                                    value={item.overview.median_owners}
-                                />
-                            </td>
-                        ))}
-                    </tr>
-                    <tr>
-                        <td>Max Revenue</td>
-                        {data.map((item, index) => (
-                            <td key={index}>
-                                $
-                                <NumberFormatter
-                                    value={getSpecificRevenue(item.overview, 1)}
-                                />
-                            </td>
-                        ))}
-                    </tr>
-                    <tr>
-                        <td>Q3 Revenue</td>
-                        {data.map((item, index) => (
-                            <td key={index}>
-                                $
-                                <NumberFormatter
-                                    value={getSpecificRevenue(
-                                        item.overview,
-                                        0.75
-                                    )}
-                                />
-                            </td>
-                        ))}
-                    </tr>
-                    <tr>
-                        <td>Median Revenue</td>
-                        {data.map((item, index) => (
-                            <td key={index}>
-                                $
-                                <NumberFormatter
-                                    value={getSpecificRevenue(
-                                        item.overview,
-                                        0.5
-                                    )}
-                                />
-                            </td>
-                        ))}
-                    </tr>
-                    <tr>
-                        <td>Q1 Revenue</td>
-                        {data.map((item, index) => (
-                            <td key={index}>
-                                $
-                                <NumberFormatter
-                                    value={getSpecificRevenue(
-                                        item.overview,
-                                        0.25
-                                    )}
-                                />
-                            </td>
-                        ))}
-                    </tr>
-                    <tr>
-                        <td>Min Revenue</td>
-                        {data.map((item, index) => (
-                            <td key={index}>
-                                $
-                                <NumberFormatter
-                                    value={getSpecificRevenue(item.overview, 0)}
-                                />
-                            </td>
-                        ))}
-                    </tr>
-                </tbody>
-            </table>
+            <div className="table-responsive">
+                <table className="table table-striped">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            {data.map((item, index) => (
+                                <th key={index}>{item.title}</th>
+                            ))}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Games Released</td>
+                            {data.map((item, index) => (
+                                <td key={index}>
+                                    <NumberFormatter
+                                        value={item.overview.total_games}
+                                    />
+                                </td>
+                            ))}
+                        </tr>
+                        <tr>
+                            <td>Median Reviews</td>
+                            {data.map((item, index) => (
+                                <td key={index}>
+                                    <NumberFormatter
+                                        value={item.overview.median_reviews}
+                                    />
+                                </td>
+                            ))}
+                        </tr>
+                        <tr>
+                            <td>Median Owners</td>
+                            {data.map((item, index) => (
+                                <td key={index}>
+                                    <NumberFormatter
+                                        value={item.overview.median_owners}
+                                    />
+                                </td>
+                            ))}
+                        </tr>
+                        <tr>
+                            <td>Max Revenue</td>
+                            {data.map((item, index) => (
+                                <td key={index}>
+                                    $
+                                    <NumberFormatter
+                                        value={getSpecificRevenue(
+                                            item.overview,
+                                            1
+                                        )}
+                                    />
+                                </td>
+                            ))}
+                        </tr>
+                        <tr>
+                            <td>Q3 Revenue</td>
+                            {data.map((item, index) => (
+                                <td key={index}>
+                                    $
+                                    <NumberFormatter
+                                        value={getSpecificRevenue(
+                                            item.overview,
+                                            0.75
+                                        )}
+                                    />
+                                </td>
+                            ))}
+                        </tr>
+                        <tr>
+                            <td>Median Revenue</td>
+                            {data.map((item, index) => (
+                                <td key={index}>
+                                    $
+                                    <NumberFormatter
+                                        value={getSpecificRevenue(
+                                            item.overview,
+                                            0.5
+                                        )}
+                                    />
+                                </td>
+                            ))}
+                        </tr>
+                        <tr>
+                            <td>Q1 Revenue</td>
+                            {data.map((item, index) => (
+                                <td key={index}>
+                                    $
+                                    <NumberFormatter
+                                        value={getSpecificRevenue(
+                                            item.overview,
+                                            0.25
+                                        )}
+                                    />
+                                </td>
+                            ))}
+                        </tr>
+                        <tr>
+                            <td>Min Revenue</td>
+                            {data.map((item, index) => (
+                                <td key={index}>
+                                    $
+                                    <NumberFormatter
+                                        value={getSpecificRevenue(
+                                            item.overview,
+                                            0
+                                        )}
+                                    />
+                                </td>
+                            ))}
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </small>
     );
 }
