754c3e3a31ac351aecb1449af3dcc7e18a0fa8c6
Lev Kobelev <35657934+evlko@users.noreply.github.com>
2024-08-16 21:19:02 +0300

    [fix] navbar

--- steam-trender/src/components/Navbar.tsx
+++ steam-trender/src/components/Navbar.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
+    const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+
+    const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
+
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light sticky-top">
             <div className="container-fluid px-3 py-1">
@@ -12,30 +16,31 @@ const Navbar = () => {
                     </b>
                 </Link>
                 <button
-                    className="navbar-toggler"
+                    className="custom-toggler navbar-toggler"
                     type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent"
-                    aria-expanded="false"
+                    data-toggle="collapse"
+                    data-target="#navbarContent"
+                    aria-controls="navbarContent"
+                    aria-expanded={!isNavCollapsed ? true : false}
                     aria-label="Toggle navigation"
+                    onClick={handleNavCollapse}
                 >
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div
-                    className="collapse navbar-collapse"
-                    id="navbarSupportedContent"
+                    className={`${isNavCollapsed ? "collapse" : ""} navbar-collapse`}
+                    id="navbarContent"
                 >
                     <ul className="navbar-nav">
                         <li className="nav-item">
                             <NavLink
-                                to="/description"
+                                to="/faq"
                                 className={({ isActive }) =>
                                     "navbar-brand text-uppercase" +
                                     (isActive ? " active-link" : "")
                                 }
                             >
-                                Description
+                                FAQ
                             </NavLink>
                         </li>
                         <li className="nav-item">
@@ -72,6 +77,14 @@ const Navbar = () => {
                             </NavLink>
                         </li>
                     </ul>
+                    <a
+                        href="https://www.paypal.com/donate?business=YOUR_PAYPAL_EMAIL"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="btn btn-outline-primary ms-auto text-uppercase"
+                    >
+                        Buy Me a Coffee
+                    </a>
                 </div>
             </div>
         </nav>
