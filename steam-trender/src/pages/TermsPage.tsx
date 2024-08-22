7e75ac7b1a22267ed87f4d2ad9fad0dbd2abd991
Lev Kobelev <35657934+evlko@users.noreply.github.com>
2024-08-22 22:34:29 +0300

    [add] privacy & tos

---
+++ steam-trender/src/pages/TermsPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Markdown } from "../components/Markdown";
+
+const TermsPage = () => {
+    return (
+        <>
+            <Markdown file={"terms"} />
+        </>
+    );
+};
+
+export default TermsPage;
