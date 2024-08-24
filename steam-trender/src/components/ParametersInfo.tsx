636b1ac917fe5c2af98d6f52a8a47a84f78989f5
Lev Kobelev <35657934+evlko@users.noreply.github.com>
2024-08-24 08:27:17 +0300

    [fix] params info

--- steam-trender/src/components/ParametersInfo.tsx
+++ steam-trender/src/components/ParametersInfo.tsx
@@ -3,11 +3,11 @@ import React from "react";
 export const ParametersInfo = () => {
     return (
         <p>
-            If you see this message, it means either you have not selected
-            the parameters yet, or you have selected them in such a way that a
-            null result is returned. Here is a brief description of the
-            params for this page & their (default values), required
-            are marked with<span className="text-primary fw-bold">*</span>.
+            If you see this message, it means either you have not selected the
+            parameters yet, or you have selected them in such a way that a null
+            result is returned. Here is a brief description of the params for
+            this page & their (default values), required are marked with
+            <span className="text-primary fw-bold">*</span>.
         </p>
     );
 };
