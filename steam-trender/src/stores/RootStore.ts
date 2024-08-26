b80debf8651f5cdf6bfd5d839b7c34308d8e83e2
Lev Kobelev <35657934+evlko@users.noreply.github.com>
2024-08-26 23:38:58 +0300

    [add] root store

--- steam-trender/src/stores/RootStore.ts
+++ steam-trender/src/stores/RootStore.ts
@@ -1,12 +1,15 @@
+import { TagsPageStore } from "./TagsPageStore";
 import { TagsStore } from "./TagsStore";
 import { TrendsPageStore } from "./TrendsPageStore";
 
 export class RootStore {
     trendsPageStore: TrendsPageStore;
     tagsStore: TagsStore;
+    tagsPageStore: TagsPageStore;
 
     constructor() {
         this.trendsPageStore = new TrendsPageStore(this);
+        this.tagsPageStore = new TagsPageStore(this);
         this.tagsStore = new TagsStore(this);
     }
 }
