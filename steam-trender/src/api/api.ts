f6e14359c9d363a6775fa400cf303264c483df47
Lev Kobelev <35657934+evlko@users.noreply.github.com>
2024-08-30 00:23:11 +0300

    [add] withCredentials to api

--- steam-trender/src/api/api.ts
+++ steam-trender/src/api/api.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
     baseURL: process.env.REACT_APP_API_URL || "http://api:8000",
+    withCredentials: true,
     headers: {
         "Content-Type": "application/json",
     },
