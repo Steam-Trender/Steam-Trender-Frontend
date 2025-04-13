13f0a1460016ecbe79dae86309064b6434d88e5c
Lev Kobelev <35657934+evlko@users.noreply.github.com>
2025-04-13 18:59:59 +0300

    [add] offline font

--- steam-trender/src/index.tsx
+++ steam-trender/src/index.tsx
@@ -5,8 +5,11 @@ import App from "./App";
 import "./styles/custom.css";
 import "./styles/main.css";
 
-const root = ReactDOM.createRoot(
-    document.getElementById("root") as HTMLElement
-);
-
-root.render(<App />);
+document.fonts.load("1em \"Libre Barcode 128 Text\"").then(() => {
+    document.fonts.ready.then(() => {
+        const root = ReactDOM.createRoot(
+            document.getElementById("root") as HTMLElement
+        );
+        root.render(<App />);
+    });
+});
